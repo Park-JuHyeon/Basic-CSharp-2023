0ec1be876a1b79da7033676a8e5fbabdce8c503b
Park-JuHyeon <j-hyeon97@naver.com>
2023-04-04 12:13:13 +0900

    C# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!!");     // System안에 문자를 출력할수 있는 함수가 있다!
+                                                 // 하지만 위에 using으로 system. 글자를 따로 쓰지 않아도 된다.
+        }
+    }
+}
