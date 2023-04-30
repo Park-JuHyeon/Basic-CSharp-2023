58f7b8dcbc807c38656bdd00e51e4030f7244c04
Park-JuHyeon <j-hyeon97@naver.com>
2023-04-30 22:39:44 +0900

    asd

---
+++ HomePractice/test01/test01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+
+    class Parent
+    {
+        public string name = "ppp";
+        public Parent() {
+            Console.WriteLine("parent");
+        }
+        public virtual void info()
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    class Child : Parent
+    {
+        public string name = "ccc";
+        public Child()
+        {
+            Console.WriteLine("child");   
+        }
+
+        public override void info()
+        {
+            base.info();
+            Console.WriteLine(name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Child();
+            p.info();
+        }
+    }
+}
